6bc3678fae6d0046efc1c1cb0494500808d7de49
elli turner-bisset <ETurner-Bisset@users.noreply.github.com>
2023-11-20 21:40:01 +0000

    update to dbConfig

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { Pool } from "pg";
+import pkg from 'pg';
+const { Pool } = pkg;
 
 const isProduction = process.env.NODE_ENV === "production";
 
