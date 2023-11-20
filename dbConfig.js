571c79eacb4462c9ec2c450398343b4a80be57f2
elli turner-bisset <ETurner-Bisset@users.noreply.github.com>
2023-11-20 21:28:19 +0000

    update to dbConfig

--- dbConfig.js
+++ dbConfig.js
@@ -8,8 +8,15 @@ const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 
 export const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: true
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    database: process.env.PGDATABASE, 
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    rejectUnauthorized: true,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 15000
 });
 
 export default pool;
