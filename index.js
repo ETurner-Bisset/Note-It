ba03d6e06b3402b1db0cf5b7d62cda82a6f1171b
elli turner-bisset <ETurner-Bisset@users.noreply.github.com>
2023-12-15 14:05:34 +0000

    Add tones of functionality

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ import flash from "express-flash";
 import session from "express-session";
 import bcrypt from "bcrypt";
 import intializePassport from "./passportConfig.js";
+import ejsLint from "ejs-lint";
+
+ejsLint("ejs", {async: true});
 
 intializePassport(passport);
 
@@ -15,6 +18,8 @@ const app = express();
 const port =  8080;
 
 const date = new Date().getFullYear();
+
+let done = false;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -43,15 +48,35 @@ app.get("/login", checkAuthenticated, (req, res) => {
 
 app.get("/main", ensureAuthenticated, async (req, res) => {
     const id = req.user.id;
-    // console.log(id);
-    const notes = await pool.query("SELECT * FROM notes WHERE user_id = $1 ORDER BY id ASC", [id]);
-    console.log(notes.rows);
-    res.render("main.ejs", {date: date, notes: notes.rows});
+    const noteTitles = await pool.query("SELECT * FROM notes WHERE user_id = $1", [id])
+    const listItems = await pool.query("SELECT list_item, note_id FROM items JOIN notes ON notes.id = $1", [id]);
+    
+    // console.log(noteTitles.rows);
+    res.render("main-v2.ejs", {date: date, noteTitles: noteTitles.rows, listItems: listItems.rows});
 });
 
+// Example note
+app.get("/exampleNote", (req, res) => {
+    if (req.isAuthenticated() ) {
+        res.render("exampleNote.ejs", {date: date});
+    } else {
+        res.redirect("/login");
+    }
+})
+
 app.get("/note", (req, res) => {
+    const id = req.user.id;
     if (req.isAuthenticated()) {
-        res.render("note.ejs", {date: date});
+        res.render("note.ejs", {date: date, id: id});
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/voiceNote", (req, res) => {
+    const id = req.user.id;
+    if (req.isAuthenticated()) {
+        res.render("voiceNote.ejs", {date: date, id: id});
     } else {
         res.redirect("/login");
     }
@@ -131,49 +156,150 @@ app.post("/login", passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true
 }));
+// show single note
+app.post("/showNote", async (req, res) => {
+    const noteId = req.body.noteId;
+    const noteTitle = await pool.query("SELECT note_title FROM notes WHERE id = $1", [noteId])
+    const listItems = await pool.query("SELECT list_item, id, done, note_id FROM items WHERE note_id = $1 ORDER BY id ASC", [noteId]);
+    // console.log(listItems.rows);
+    const noteBody = await pool.query("SELECT * FROM voicenotes WHERE note_id = $1", [noteId]);
+    // console.log(noteBody.rows[0]);
+    res.render("showNote.ejs", {date: date, noteTitle: noteTitle.rows[0].note_title, listItems: listItems.rows, noteId: noteId, noteBody: noteBody.rows[0]});
+});
     
-app.post("/note", (req, res) => {
+app.post("/note", async (req, res) => {
     const id = req.user.id;
-    const note = req.body.note;
+    const noteTitle = req.body.listTitle;
+    const items = [...req.body.item];
+    // console.log(items);
     try {
-        pool.query("INSERT INTO notes (notes, user_id) VALUES ($1, $2)", [note, id]);
+        const noteId = await pool.query("INSERT INTO notes (note_title, user_id) VALUES ($1, $2) RETURNING id", [noteTitle, id]);
+        items.forEach(async (item) => {
+            if (item !== "") {
+                const listItems = await pool.query("INSERT INTO items (list_item, note_id, user_id, done) VALUES ($1, $2, $3, $4) RETURNING list_item", [item, noteId.rows[0].id, id, done]);
+            // console.log(listItems.rows);
+            }
+        })
         res.redirect("/main");
     } catch (error) {
         console.log(error);
     }
 });
 
+// Add voice note
+app.post("/voiceNote", async (req, res) => {
+    const id = req.user.id;
+    const noteTitle = req.body.noteTitle;
+    const noteBody = req.body.noteBody;
+    try {
+        const noteId = await pool.query("INSERT INTO notes (note_title, user_id) VALUES ($1, $2) RETURNING id", [noteTitle, id]);
+        console.log(noteId);
+        await pool.query("INSERT INTO items ( list_item, note_id, user_id, done) VALUES ($1, $2, $3, $4)", [noteBody, noteId.rows[0].id, id, done]);
+        res.redirect("/main");
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// Add single item
+app.post("/add", ensureAuthenticated, async (req, res) => {
+    const noteId = req.body.noteId;
+    const id = req.user.id;
+    const item = req.body.item;
+    console.log(noteId);
+    try {
+        pool.query("INSERT INTO items (list_item, note_id, user_id, done) VALUES ($1, $2, $3, $4)", [item, noteId, id, done]);
+        const noteTitle = await pool.query("SELECT note_title FROM notes WHERE id = $1 ORDER BY id ASC", [noteId]);
+        const listItems = await pool.query("SELECT list_item, id FROM items WHERE note_id = $1", [noteId]);   
+        res.render("showNote.ejs", {date: date, noteTitle: noteTitle.rows[0].note_title, listItems: listItems.rows, noteId: noteId});
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// Edit single item
 app.post("/edit", async (req, res) => {
-    const noteId = req.body.noteId;
-    // console.log(noteId);
-    const note = await pool.query("SELECT notes FROM notes WHERE id = $1", [noteId]);
-    console.log(note.rows[0].notes);
-    if (req.isAuthenticated()) {
-        res.render("edit.ejs", {date: date, note: note.rows[0].notes, noteId: noteId});
-    } else {
-        res.redirect("/login");
-    }
+   const itemId = req.body.updatedItemId;
+   const updatedItem = req.body.updatedItem;
+   const noteId = req.body.noteId;
+   console.log(noteId);
+   try {
+    await pool.query("UPDATE items SET list_item = $1 WHERE id = $2", [updatedItem, itemId]);
+    const noteTitle = await pool.query("SELECT note_title FROM notes WHERE id = $1 ORDER BY id ASC", [noteId]);
+    const listItems = await pool.query("SELECT list_item, id FROM items WHERE note_id = $1 ORDER BY id ASC", [noteId]);   
+    res.render("showNote.ejs", {date: date, noteTitle: noteTitle.rows[0].note_title, listItems: listItems.rows, noteId: noteId});
+   } catch (error) {
+    console.log(error);
+   }
 });
 
-app.post("/editNote", (req, res) => {
-    const updatedNote = req.body.updatedNote;
-    const noteId = req.body.noteId;
+// Check off single item
+app.post("/checkOff", async (req, res) => {
     try {
-        pool.query("UPDATE notes SET notes = $1 WHERE id = $2", [updatedNote, noteId]);
-        res.redirect("/main");
+        const item = req.body.checkbox;
+        const doneStatus = await pool.query("UPDATE items SET done = NOT done WHERE list_item = $1 RETURNING done", [item]);
+        // console.log(doneStatus.rows[0]);
+        const noteId = req.body.noteId;
+        const noteTitle = await pool.query("SELECT note_title FROM notes WHERE id = $1", [noteId])
+        const listItems = await pool.query("SELECT list_item, id, done, note_id FROM items WHERE note_id = $1 ORDER BY id ASC", [noteId]);
+        // console.log(listItems.rows);
+        const noteBody = await pool.query("SELECT * FROM voicenotes WHERE note_id = $1", [noteId]);
+        // console.log(noteBody.rows[0]);
+        res.render("showNote.ejs", {date: date, noteTitle: noteTitle.rows[0].note_title, listItems: listItems.rows, noteId: noteId, noteBody: noteBody.rows[0], doneStatus: doneStatus.rows[0].done});
+        } catch (error) {
+            console.log(error);
+        }
+})
+
+// Delete single item
+app.post("/delete", async (req, res) => {
+    const itemId = req.body.itemId;
+    const noteId = await pool.query("SELECT note_id FROM items WHERE ID = $1", [itemId]);
+    
+    try {
+        await pool.query("DELETE FROM items WHERE id = $1", [itemId]);
+        const noteTitle = await pool.query("SELECT note_title FROM notes WHERE id = $1 ", [noteId.rows[0].note_id]);
+        const listItems = await pool.query("SELECT list_item, id, done FROM items WHERE note_id = $1 ORDER BY id ASC", [noteId.rows[0].note_id]);
+        res.render("showNote.ejs", {date: date, noteTitle: noteTitle.rows[0].note_title, listItems: listItems.rows, noteId: noteId});
     } catch (error) {
         console.log(error);
     }
-});
-
-app.post("/delete", (req, res) => {
+})
+//  Delete whole note
+app.post("/delete-note", async (req, res) => {
     const noteId = req.body.noteId;
     try {
-       pool.query("DELETE FROM notes WHERE id = $1", [noteId]);
-       res.redirect("/main");
+        await pool.query("DELETE FROM items WHERE note_id = $1", [noteId]);
+        await pool.query("DELETE FROM notes WHERE id = $1", [noteId]);
+        res.redirect("/main");
     } catch (error) {
-       console.log(error); 
+        console.log(error); 
     }
+});
+
+// search note title 
+app.post("/search", async (req, res) => {
+    const searchtitle = req.body.searchTitle;
+    const searchedTitle = await pool.query("SELECT note_title FROM notes WHERE note_title = $1", [searchtitle]);
+    // console.log(searchedTitle.rows.length);
+    if (searchedTitle.rows.length < 1) {
+        const noteId = await pool.query("SELECT id FROM notes WHERE note_title = $1", [searchtitle])
+        // console.log(noteId.rows);
+        res.render("showNote.ejs", {date: date, noteId: noteId.rows})
+    } else {
+        try {
+            const noteId = await pool.query("SELECT id FROM notes WHERE note_title = $1", [searchtitle])
+            console.log(noteId.rows[0].id);
+            const noteTitle = await pool.query("SELECT note_title FROM notes WHERE id = $1", [noteId.rows[0].id])
+            const listItems = await pool.query("SELECT list_item, id, done, note_id FROM items WHERE note_id = $1", [noteId.rows[0].id]);
+            const noteBody = await pool.query("SELECT * FROM voicenotes WHERE note_id = $1", [noteId.rows[0].id])
+            res.render("showNote.ejs", {date: date, noteTitle: noteTitle.rows[0].note_title, listItems: listItems.rows, noteId: noteId, noteBody: noteBody.rows[0]}); 
+        
+    } catch (error) {
+        console.log(error);
+    }
+    }
+    
 });
 
 function ensureAuthenticated(req, res, next) {
@@ -197,6 +323,11 @@ function checkAuthenticated(req, res, next) {
     }
     res.redirect("/login");
   }
+
+async function getHighestId(arr) {
+    arr.sort((a, b) => b - a);
+    return arr[0];
+};
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
