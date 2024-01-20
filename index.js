ed2fb14d6f5b5256c38a5c9e5588a1946ec231d2
elli turner-bisset <ETurner-Bisset@users.noreply.github.com>
2024-01-20 14:28:59 +0000

    Add delete all checked items

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ ejsLint('ejs', { async: true });
 intializePassport(passport);
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 
