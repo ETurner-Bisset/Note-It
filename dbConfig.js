c01e35abdbbe3823784c3388fee880f3ca56f2f7
elli turner-bisset <ETurner-Bisset@users.noreply.github.com>
2024-01-20 14:24:18 +0000

    Add delete all checked items

--- dbConfig.js
+++ dbConfig.js
@@ -13,9 +13,9 @@ export const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  // ssl:{
-  //     require: true,
-  // },
+  ssl: {
+    require: true,
+  },
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 15000,
